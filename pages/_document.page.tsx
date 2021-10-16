3bbb4a0be16426e4b9663478a173994386b2bb52
John Datserakis <johndatserakis@gmail.com>
2021-10-15 21:29:18 -0400

    Update _document

--- pages/_document.page.tsx
+++ pages/_document.page.tsx
@@ -1,10 +1,11 @@
-import Document, { Html, Main, NextScript } from 'next/document';
+import Document, { Head, Html, Main, NextScript } from 'next/document';
 import { ServerStyleSheet } from 'styled-components';
 
 export default class MyDocument extends Document {
   render() {
     return (
       <Html lang="en">
+        <Head />
         <body>
           <Main />
           <NextScript />
